bbe28de629ec570f4dfd4bfad89e02edb37a7914
Isael Requena <isaeljafeth08@gmail.com>
2023-05-09 13:36:37 -0400

    my base project

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HammerModule,
+    HttpClientModule
+    ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+})
+export class AppModule {}
---
+++ src/app/interfaces/Homework.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+export interface HomeworkInterface {
+  id_homeworks?: number;
+  homework_title: string;
+  homework_description: string;
+  homework_done: number;
+  homework_start_date: Date;
+  homework_end_date: string;
+  school_subject: string;
+}

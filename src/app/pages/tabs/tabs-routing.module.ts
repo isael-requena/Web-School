98ff18a3905f435be431a26cf08c1b84da47650e
Isael Requena <isaeljafeth08@gmail.com>
2023-05-28 01:28:36 -0400

    login design in progress

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -9,11 +9,6 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: '**',
-        redirectTo: 'home',
-        pathMatch: 'full'
-      },
-      {
         path: 'home',
         loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
       },
